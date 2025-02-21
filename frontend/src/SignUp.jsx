ceaaaec82e76f0f0823d74f4ed667a918963c684
hayyanhaider0 <hayyan2003@gmail.com>
2025-02-21 14:53:41 -0600

    build removed from ignore

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -1,23 +1,26 @@
 const SignUp = () => {
 	return (
-		<section>
+		<section className='flex flex-col gap-4 w-96 border-2 border-black rounded-3xl items-center justify-center p-8'>
 			<h1>Welcome to DarkDrive</h1>
-			<p>Login and upload your files, safe and secure, completely free!</p>
+			<p className='text-center'>Login and upload your files, safe and secure, completely free!</p>
 			<hr />
-			<span>
+			<span className='flex flex-col gap-2 w-full'>
 				<label>Email</label>
 				<input placeholder='example@email.com' type='email' />
 			</span>
-			<span>
+			<span className='flex flex-col gap-2 w-full'>
 				<label>Password</label>
 				<input
 					placeholder='&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;'
-					type='email'
+					type='password'
 				/>
 			</span>
-			<span>
+			<span className='flex flex-col gap-2 w-full'>
 				<label>Confirm Password</label>
-				<input placeholder='example@email.com' type='email' />
+				<input
+					placeholder='&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;'
+					type='password'
+				/>
 			</span>
 		</section>
 	);
