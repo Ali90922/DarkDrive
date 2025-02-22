6ea6b6ae04f4f5291a57f72e574b056a5b9f4efb
Ali Nawaz <150611589+Ali90922@users.noreply.github.com>
2025-02-22 06:21:52 -0600

    Update metrics_api.py

--- BackEnd/metrics_api.py
+++ BackEnd/metrics_api.py
@@ -48,6 +48,6 @@ async def metrics():
     return get_system_metrics()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=6000)    
+    uvicorn.run(app, host="0.0.0.0", port=8080)    
 o
 
